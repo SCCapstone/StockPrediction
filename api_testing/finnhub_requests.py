c7cd7a89c0a6ba297630df25b90fadd31846fb2c
AaronTheNerd <bargeaaron@gmail.com>
2020-11-18 13:26:28 -0500

    showed decoding of request response

--- api_testing/finnhub_requests.py
+++ api_testing/finnhub_requests.py
@@ -7,4 +7,10 @@ symbols = ['AMZN', 'AAPL', 'F', 'UA', 'GOOG']
 for symbol in symbols:
     r = requests.get('https://finnhub.io/api/v1/quote?symbol=' + symbol + '&token=' + API_KEY)
     print(symbol)
-    print(r.json())
+    price_map = r.json()
+    print("Current: " + str(price_map['c']))
+    print("High: " + str(price_map['h']))
+    print("Low: " + str(price_map['l']))
+    print("Open: " + str(price_map['o']))
+    print("Previous Close: " + str(price_map['pc']))
+    print("Timestamp: " + str(price_map['t']))

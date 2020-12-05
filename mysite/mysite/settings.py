e546a08f75c8a8f7f61a521e82bb5972e9081b46
Max Corbel <mcorbel@email.sc.edu>
2020-12-05 02:14:13 -0500

    Fixing deployment

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$5)6v9#zg&p!l*h_o^3(208!r*j0*spe&^q(#_j-5_bn$39n$m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://stock-prediction1.herokuapp.com/']
+ALLOWED_HOSTS = ['stock-prediction1.herokuapp.com', '127.0.0.1']
 
 
 # Application definition

4ccfc39cf9bb3eb57c7f3c58fbe73137ddb48eef
Miles <mziemer@email.sc.edu>
2021-03-05 15:41:08 -0500

    corrected fetch url heroku

--- stockpre-web/src/ENV.js
+++ stockpre-web/src/ENV.js
@@ -1,3 +1,3 @@
-//const HOSTNAME = "https://afternoon-anchorage-50870.herokuapp.com/api";
-const HOSTNAME = "http://127.0.0.1:8000/api";
+const HOSTNAME = "https://stock-prediction-1.herokuapp.com/api";
+//const HOSTNAME = "http://127.0.0.1:8000/api";
 export default HOSTNAME;

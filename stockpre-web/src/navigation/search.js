2b6a4b51169544522ba542430108358843a97124
AaronTheNerd <bargeaaron@gmail.com>
2021-03-22 20:01:07 -0400

    Implemented datalist in search bar

--- stockpre-web/src/navigation/search.js
+++ stockpre-web/src/navigation/search.js
@@ -6,7 +6,6 @@ import { apiStockSearch } from "./lookup";
 export function StockSearchComponent(props) {
   const [searchTerm, setSearchTerm] = useState("");
   const [searchResults, setSearchResults] = useState([]);
-  const [showResults, setShowResults] = useState(true);
 
   const handleSearchTermChange = (event) => {
     setSearchTerm(event.target.value);
@@ -27,19 +26,19 @@ export function StockSearchComponent(props) {
 
   return (
     <form className="d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
-      <div className="input-group" onFocus={() => setShowResults(true)}>
+      <div className="input-group">
         <input
           type="text"
           className="form-control bg-light border-0 small z-index:10"
           placeholder="Search tickers"
           value={searchTerm}
           onChange={handleSearchTermChange}
-          list="list"
+          list="stocks"
         />
-        <datalist id="list" className="position-absolute mt-5 border border-light bg-light">
-          {searchTerm !== "" && showResults && searchResults.map((item) => (
-                  <option value={item} />
-              ))}
+        <datalist id="stocks">
+          {searchTerm !== "" && searchResults.map((item) => (
+            <option value={item.ticker} /> 
+          ))}
         </datalist>
       </div>
     </form>

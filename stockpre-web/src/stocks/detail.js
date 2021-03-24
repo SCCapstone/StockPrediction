40eab97c2ca4c9efe3f79f06a20bf4a486a282b2
AaronTheNerd <bargeaaron@gmail.com>
2021-03-23 23:28:40 -0400

    Finished modifcation of TV chart

--- stockpre-web/src/stocks/detail.js
+++ stockpre-web/src/stocks/detail.js
@@ -8,6 +8,12 @@ function getLanguageFromURL() {
 	const results = regex.exec(window.location.search);
 	return results === null ? null : decodeURIComponent(results[1].replace(/\+/g, ' '));
 }
+function americanDateToUnixTimestamp(date) {
+	// mm/dd/yyyy to yyyy-mm-dd to Javascript timestamp (ms) to Unix timestamp (s)
+	let fields = date.split('/');
+	date = fields[2] + '-' + fields[0] + '-' + fields[1];
+	return new Date(date).getTime() / 1000;
+}
 const defaultSettings = {
 	symbol: 'AAPL',
 	interval: 'D',
@@ -37,6 +43,8 @@ const defaultSettings = {
 		]);
 	}
 };
+let currPrediction = null;
+let tvWidget = null;
 export function Stock(props) {
 	const {
 		symbol,
@@ -44,7 +52,6 @@ export function Stock(props) {
 		didPredictionLookup,
 		handleBackendPredictionLookup
 	} = props;
-	let tvWidget = null;
 	useEffect(() => {
 		const widgetOptions = {
 			symbol: symbol ? symbol : defaultSettings.symbol, // Could throw warning here
@@ -53,9 +60,22 @@ export function Stock(props) {
 			interval: defaultSettings.interval,
 			container_id: defaultSettings.containerId,
 			library_path: defaultSettings.libraryPath,
-
 			locale: getLanguageFromURL() || 'en',
-			disabled_features: ['use_localstorage_for_settings', 'left_toolbar', 'header_symbol_search', 'display_market_status', 'header_screenshot', 'header_compare', 'header_indicators', 'compare_symbol', 'header_saveload', 'create_volume_indicator_by_default', 'control_bar', 'show_chart_property_page', 'countdown',],
+			disabled_features: [
+				'use_localstorage_for_settings', 
+				'left_toolbar', 
+				'header_symbol_search', 
+				'display_market_status', 
+				'header_screenshot', 
+				'header_compare', 
+				'header_indicators', 
+				'compare_symbol', 
+				'header_saveload', 
+				'create_volume_indicator_by_default', 
+				'control_bar', 
+				'show_chart_property_page', 
+				'countdown'
+			],
 			enabled_features: ['study_templates'],
 			charts_storage_url: defaultSettings.chartsStorageUrl,
 			charts_storage_api_version: defaultSettings.chartsStorageApiVersion,
@@ -66,13 +86,93 @@ export function Stock(props) {
 			studies_overrides: defaultSettings.studiesOverrides,
 		};
 		tvWidget = new widget(widgetOptions);
-		if (didPredictionLookup === false) {
+		if (didPredictionLookup === false && !currPrediction) {
 		  apiPredictionLookup(symbol, handleBackendPredictionLookup);
 		};
-		if (prediction) {
-			console.log(prediction);
-		} else {
-			console.log("no pred");
+		if (prediction && !currPrediction) {
+			currPrediction = prediction;
+			console.log("prediction set")
+			console.log(currPrediction);
+		}
+		if (currPrediction !== null && tvWidget !== null) {
+			const currTime = new Date("03/27/2018").getTime() / 1000;
+			//currPrediction.prediction_date
+			const predTime = americanDateToUnixTimestamp("05/01/2018");
+			tvWidget.onChartReady(() => {
+				const chart = tvWidget.chart();
+				const {from, to} = chart.getVisibleRange();
+				chart.setVisibleRange({from: from, to: predTime});
+				try {
+					chart.createMultipointShape(
+						[{
+							time: currTime,
+							channel: 'close'
+						}, 
+						{
+							time: predTime,
+							price: 14.10,
+							channel: 'close'
+						}], 
+						{
+							shape: 'trend_line',
+							lock: true,
+							overrides: {
+								linecolor: '#00ff00',
+								linestyle: 2
+							}
+						}, null
+					);
+				} catch (err) {
+					console.log(err);
+				}
+				try {
+					chart.createMultipointShape(
+						[{
+							time: currTime,
+							channel: 'close'
+						}, 
+						{
+							time: predTime, 
+							price: currPrediction.lower_value,
+							channel: 'close'
+						}], 
+						{
+							shape: 'trend_line', 
+							lock: true,
+							overrides: {
+								linecolor: '#ff0000',
+								linestyle: 2
+							}
+						}, null
+					);
+				} catch (err) {
+					console.log(err);
+				}
+				try {
+					chart.createMultipointShape(
+						[{
+							time: currTime + 1,
+							channel: 'close'
+						}, 
+						{
+							time: predTime, 
+							price: 13.6,
+							channel: 'close'
+						}], 
+						{
+							shape: 'trend_line', 
+							lock: true,
+							overrides: {
+								linecolor: '#000000',
+								linestyle: 2
+							}
+						}, null
+					);
+				} catch (err) {
+					console.log(err);
+				}
+			});
+			
 		}
 		return () => {
 			if (tvWidget !== null) {

38b359ef22e853a49b055515bbab94b091ed817b
AaronTheNerd <bargeaaron@gmail.com>
2021-03-25 13:58:17 -0400

    Created a functional datafeed

--- stockpre-web/src/stocks/datafeed.js
+++ stockpre-web/src/stocks/datafeed.js
@@ -1,3 +1,14 @@
+import { subscribeOnStream, unsubscribeFromStream } from './streaming.js'
+const finnhub = require('finnhub');
+const request = require('request');
+
+const authToken = "bua1l2n48v6q418fsepg"; 
+
+const api_key = finnhub.ApiClient.instance.authentications['api_key'];
+api_key.apiKey = authToken;
+const finnhubClient = new finnhub.DefaultApi();
+
+const lastBarsCache = new Map();
 
 const configurationData = {
     supported_resolutions: ['1D', '1W', '1M'], // Will need to check if these resolutions are valid
@@ -6,16 +17,11 @@ const configurationData = {
             value: 'NYSE',
             name: 'NYSE',
             desc: 'New York Stock Exchange'
-        },
-        {
-            value: 'NASDAQ',
-            name: 'NASDAQ',
-            desc: 'NASDAQ Stock Exchange'
         }
     ],
     symbol_types: [
         {
-            name: 'stock',
+            name: 'Common Stock',
             value: 'stock'
         }
     ]
@@ -32,7 +38,32 @@ export default {
         onSymbolResolvedCallback,
         onResolveErrorCallback
     ) => {
-        
+        const symbols = await (await fetch(`https://finnhub.io/api/v1/stock/symbol?exchange=US&token=${authToken}`)).json();
+        console.log("SYMBOLS:", symbols);
+        const symbolItem = symbols.find(({ displaySymbol }) => symbolName === displaySymbol);
+        console.log("FOUND SYMBOL:", symbolItem);
+        if (!symbolItem) {
+            onResolveErrorCallback(`Cannot resolve symbol: ${symbolName}`);
+            return;
+        }
+        const symbolInfo = {
+            ticker: symbolItem.symbol,
+            name: symbolItem.displaySymbol,
+            description: symbolItem.description,
+            type: symbolItem.type,
+            session: '24x7',
+            timezone: 'Etc/UTC',
+            exchange: "NYSE",
+            minmov: 1,
+            pricescale: 100,
+            has_intraday: false,
+            has_no_volume: true,
+            has_weekly_and_monthly: false,
+            supported_resolutions: configurationData.supported_resolutions,
+            volume_precision: 2,
+            data_status: 'streaming',
+        };
+        onSymbolResolvedCallback(symbolInfo);
     },
     getBars: async (
         symbolInfo,
@@ -43,7 +74,38 @@ export default {
         onErrorCallback,
         firstDataRequest
     ) => {
+        const TV2FinnhubResolutions = {"1D":"D", "1W":"W", "1M":"M"};
+        const request = require('request');
 
+        await request(`https://finnhub.io/api/v1/stock/candle?symbol=${symbolInfo.ticker}&resolution=${TV2FinnhubResolutions[resolution]}&from=${from}&to=${to}&token=${authToken}`,
+            { json: true },
+            (error, response, data) => {
+                if (error) {
+                    onErrorCallback(error);
+                    return;
+                }
+                if (data["s"] === "no_data") {
+                    onHistoryCallback([], {noData: true});
+                    return;
+                }
+                console.log(data['t']);
+                const len = data['t'].length;
+                let bars = [];
+                for (var i = 0; i < len; i += 1) {
+                    bars = [...bars, {
+                        time: data['t'][i] * 1000,
+                        low: data['l'][i],
+                        high: data['h'][i],
+                        open: data['o'][i],
+                        close: data['c'][i]
+                    }];
+                }
+                if (firstDataRequest) {
+                    lastBarsCache.set(symbolInfo.displaySymbol, { ...bars[bars.length - 1] })
+                }
+                console.log("GETBARS:", bars);
+                onHistoryCallback(bars, {noData: false});
+        }); 
     },
     searchSymbols: async (
         userInput,
@@ -51,7 +113,7 @@ export default {
         symbolType,
         onResultReadyCallback
     ) => {
-
+        console.log("SEARCHED");
     },
     subscribeBars: (
         symbolInfo,
@@ -60,11 +122,18 @@ export default {
         subscribeUID,
         onResetCacheNeededCallback
     ) => {
-
+        subscribeOnStream(
+            symbolInfo.ticker,
+            resolution,
+            onRealtimeCallback,
+            subscribeUID,
+            onResetCacheNeededCallback,
+            lastBarsCache.get(symbolInfo.ticker)
+        );
     },
     unsubscribeBars: (
         subscriberUID
     ) => {
-
+        unsubscribeFromStream(subscriberUID);
     }
 };

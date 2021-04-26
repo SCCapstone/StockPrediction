16f25076337ab5afb695efbb7d49aeb2a1966e21
ahmedhUSC <65428832+ahmedhUSC@users.noreply.github.com>
2021-04-25 22:55:49 -0400

    Stock list look up + Buttons

--- stockpre-web/src/stocks/index.js
+++ stockpre-web/src/stocks/index.js
@@ -1,10 +1,11 @@
-import { StockDetailComponent, StockListComponent } from './components'
+import { StockDetailComponent, StockListComponent, LandingPageComponent } from './components'
 import { ActionButton, AddRemoveButton } from './buttons'
 import { apiStockLookup, apiStockList, apiStockAction, apiPredictionLookup } from './lookup'
 
 export {
     StockDetailComponent,
     StockListComponent,
+    LandingPageComponent,
     apiStockLookup,
     apiStockList,
     ActionButton,
